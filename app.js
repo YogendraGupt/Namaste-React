69f06279d8eaf7fbe8ddd675ce5d0449011ba26f
Yogendra Gupta <yogendra.gupta537@gmail.com>
2025-06-03 15:54:17 +0530

    html.index update

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-    const parent= React.createElement("div",{id: "parent"}, React.createElement("div",{id:"child"},React.createElement("h1",{id:"text"},"Hellow World.")));
-=======
+
    import React from "react";
    import ReactDOM from "react-dom/client";
-    
-    const parent= React.createElement("div",{id: "parent"}, React.createElement("div",{id:"child"},React.createElement("h1",{id:"text"},"My name is Yogendra")));
->>>>>>> 89ed4fc (Episode-01)
-    console.log(parent);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
-    console.log(parent);
+   const parent= React.createElement("div",{id: "parent"}, React.createElement("div",{id:"child"},React.createElement("h1",{id:"text"},"My name is Yogendra")));
+   const root = ReactDOM.createRoot(document.getElementById("root"));
+   root.render(parent);
+ 
